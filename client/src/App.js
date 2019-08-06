abf202db7e41b74f04109dab0c4ed93e7bfe4bff
lgxkevin <lgx9587@gmail.com>
2019-08-06 15:08:09 +1200

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Testing app</h1>
             <Link to="/">Home</Link>
           </header>
           <div>
