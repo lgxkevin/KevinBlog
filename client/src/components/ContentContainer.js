2bb0e14f6d34064ed6cedc152df3e2897bbf98a8
lgxkevin <lgx9587@gmail.com>
2019-10-30 10:48:28 +1300

    sideBar ContentContainer init

---
+++ client/src/components/ContentContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ContentContainer extends Component {
+  render() {
+    return (
+      <div>
+        contentContainer
+      </div>
+    )
+  }
+}
+
+export default ContentContainer;
