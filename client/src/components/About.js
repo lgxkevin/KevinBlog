f46272e5c5dd0e2d81b2432b206ea02c03de07a2
lgxkevin <lgx9587@gmail.com>
2019-11-01 23:04:57 +1300

    Add videoCard inside About component

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-
+//MaterialUI
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+//Components
+import VideoCard from './VideoCard';
 
 class About extends Component {
 
@@ -10,9 +17,32 @@ class About extends Component {
         <h2>
           About Myself
         </h2>
-        <div>
-          Currently working as a Web developer
-        </div>
+        <Grid container justify="space-between">
+          <Grid item xs={3}>
+            <Card >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Work experience
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Web developer in Proprius LTD
+                </Typography>
+              </CardContent>
+              <CardActions>
+              </CardActions>
+            </Card>
+
+          </Grid>
+
+          <Grid item xs={3}>
+            <VideoCard/>
+          </Grid>
+
+          <Grid item xs={4}>
+
+          </Grid>
+
+        </Grid>
       </div>
     )
   }
