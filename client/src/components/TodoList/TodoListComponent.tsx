52230d7b1614243ee5cd1eb1a0c4ffe8daee5b27
lgxkevin <lgx9587@gmail.com>
2020-01-05 17:00:37 +1300

    update todoItem

--- client/src/components/TodoList/TodoListComponent.tsx
+++ client/src/components/TodoList/TodoListComponent.tsx
@@ -7,7 +7,12 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 import {TodoItem, TodoItemTags} from "../../Interfaces/ITodoItem";
-import {mapColor} from "../../util/Common";
+import {objectMapper} from "../../util/Common";
+import {ColorMapper, IColorMapper} from "../../Interfaces/IColorMapper";
+
+type colorKeys =  "default" | "y" | "r" | "b";
+
+    // keyof typeof ColorMapper;
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,7 +34,10 @@ export default function TodoListComponent() {
 
     const OnPressEnter = (event: React.KeyboardEvent): void => {
         if (event.key === 'Enter' && todoItem !== '') {
+            updateTodoList();
+            // item created successfully
             clearInput();
+            // todo item created failed
         }
     };
 
@@ -37,28 +45,36 @@ export default function TodoListComponent() {
     const createTodoItem = (todoInput: string):TodoItem => {
         let departString = todoInput.split("/");
         let itemName = departString[0];
-        let itemTags = departString[1]? null : splitTags(departString[1]);
-
         let newTodoItem: TodoItem = {
-            name: itemName
+            name: itemName,
         };
-
+        if (departString[1]) {
+            newTodoItem.tags = splitTags(departString[1]);
+        }
+        if (departString[2]) {
+            newTodoItem.icon = departString[2];
+        }
         return newTodoItem;
     };
 
     const splitTags = (tags: string): Array<TodoItemTags> => {
         let tagsArray = tags.split(",");
-        tagsArray.map(tag => {
+        let result = tagsArray.map(tag => {
             let itemTag:TodoItemTags = {
-                tagName = tag[0],
-                color = tag[1]?null: mapColor(tag[1])
-            }
-        })
+                tagName: tag[0],
+                // todo: if tag[1] is not proper type
+                color: tag[1]?
+                    objectMapper(ColorMapper, tag[1] as colorKeys)
+                    : objectMapper(ColorMapper,'default')
+            };
+            return itemTag;
+        });
+        return result;
     };
 
     const updateTodoList = (): void => {
-
-        todoList.push(newTodoItem);
+        let newItem = createTodoItem(todoItem);
+        todoList.push(newItem);
         setTodoList(todoList);
     };
 
--- client/src/util/Common.ts
+++ client/src/util/Common.ts
@@ -1,3 +1,5 @@
 export const objectMapper = <T, K extends keyof T>(obj:T, key: K) =>{
     return obj[key];
-}
+};
+
+
